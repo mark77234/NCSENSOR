a46d32817bd1b3a4fb65ed341497a903adde2210
charmeee <mandugohigh@gmail.com>
2025-01-01 21:27:49 +0900

    style: 전역적인 스타일 지정

---
+++ lib/constants/styles.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const Color primary = Color(0xFF2C46BD);
+  static const Color secondary = Color(0xFF4B5563);
+  static const Color grey = Color(0xFFD1D5DB);
+  static const Color background = Color(0xFFFFFFFF);
+}
+
+class RadiusStyles {
+  static const BorderRadius border = BorderRadius.all(Radius.circular(8));
+}
+
+class TextStyles {
+  static const TextStyle title = TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: ColorStyles.primary);
+  static const TextStyle subtitle = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+  static const TextStyle body = TextStyle(fontSize: 14, color: Colors.black);
+}
+
+class ButtonStyles {
+  static ButtonStyle elevated = ElevatedButton.styleFrom(
+    backgroundColor: ColorStyles.primary,
+    foregroundColor: Colors.white,
+    padding: const EdgeInsets.symmetric(vertical: 16),
+    minimumSize: const Size(double.infinity, 48),
+    textStyle:  TextStyles.subtitle,
+    shape: RoundedRectangleBorder(
+      borderRadius: RadiusStyles.border,
+    ),
+  );
+  static ButtonStyle outlined = OutlinedButton.styleFrom(
+    side: const BorderSide(color: ColorStyles.primary, width: 2),
+    padding: const EdgeInsets.symmetric(vertical: 16),
+    minimumSize: const Size(double.infinity, 48),
+    textStyle:  TextStyles.subtitle,
+    shape: RoundedRectangleBorder(
+      borderRadius: RadiusStyles.border,
+    ),
+  );
+}
+
+class InputStyles {
+  static InputDecoration outlined = InputDecoration(
+    border: OutlineInputBorder(
+      borderRadius: RadiusStyles.border,
+      borderSide: BorderSide(color: ColorStyles.primary, width: 2),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: RadiusStyles.border,
+      borderSide: BorderSide(color: ColorStyles.primary, width: 2),
+    ),
+    filled: true,
+    fillColor: Colors.white,
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taesung1/constants/styles.dart';
 import 'package:taesung1/screens/splash_screen.dart';
 
 void main() {
@@ -11,6 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: ColorStyles.primary, // 앱의 기본 색상 설정
+        scaffoldBackgroundColor: ColorStyles.background, // 앱의 배경 색상 설정
+        appBarTheme: const AppBarTheme(
+          backgroundColor: ColorStyles.background, // 앱 바의 색상 설정
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: ColorStyles.background, // 바텀 네비게이션 바의 색상 설정
+          selectedItemColor: Color(0xFF2C46BD),
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
       home: const Splashscreen(), // SplashScreen 호출
     );
   }
