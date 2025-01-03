3aca9966ff09acf72160d53f1cc2dca4060fdd4b
mark <mark77234@naver.com>
2025-01-03 17:07:06 +0900

    breath_screen,alcohol_result,body_result 의 뒤로가기 버튼 제거 result 화면에 홈화면으로 돌아가는 아이콘 생성

--- lib/screens/body_result_screen.dart
+++ lib/screens/body_result_screen.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import '../constants/styles.dart';
+import 'main_screen.dart'; // MainScreen 임포트
 
 class BodyResultScreen extends StatefulWidget {
-  final String measurement; // 체취 부위
+  final String measurement;
 
   const BodyResultScreen({super.key, required this.measurement});
 
@@ -12,13 +13,11 @@ class BodyResultScreen extends StatefulWidget {
 }
 
 class _BodyResultScreenState extends State<BodyResultScreen> {
-
   @override
   Widget build(BuildContext context) {
     String resultMessage;
     int stage = Random().nextInt(5) + 1; // 1부터 5까지 랜덤으로 선택
 
-    // 체취 단계에 따른 메시지 설정
     switch (stage) {
       case 1:
         resultMessage = '쾌적한 수준';
@@ -45,6 +44,21 @@ class _BodyResultScreenState extends State<BodyResultScreen> {
           "체취 측정 결과",
           style: TextStyles.title,
         ),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: Icon(
+            Icons.home,
+            color: ColorStyles.primary,
+            size:30.0,
+          ), // 홈 아이콘
+          onPressed: () {
+            Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const MainScreen()), // MainScreen으로 이동
+                  (Route<dynamic> route) => false, // 모든 이전 페이지를 스택에서 제거
+            );
+          },
+        ),
       ),
       body: Center(
         child: Column(
@@ -55,17 +69,14 @@ class _BodyResultScreenState extends State<BodyResultScreen> {
               style: TextStyle(fontSize: 24),
             ),
             const SizedBox(height: 20),
-            // 단계에 따른 메시지 표시
             Text(
               resultMessage,
               style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 40),
-            // 1부터 5까지의 큰 숫자와 반투명 동그라미 이동
             Stack(
               alignment: Alignment.center,
               children: [
-                // 동그라미 배경
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: List.generate(5, (index) {
@@ -76,7 +87,7 @@ class _BodyResultScreenState extends State<BodyResultScreen> {
                         shape: BoxShape.circle,
                         color: index < stage
                             ? Colors.blue
-                            : Colors.grey.withValues(alpha: 0.3),
+                            : Colors.grey.withOpacity(0.3),
                       ),
                       child: Center(
                         child: Text(
