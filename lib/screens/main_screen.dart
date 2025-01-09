98e6c987e34d2a4b4c02d38ff4f04cd0b6864176
charmeee <mandugohigh@gmail.com>
2025-01-10 00:19:33 +0900

    feat: body safearea로 감싸서 ui보호

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -58,7 +58,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_selectedIndex].widget,
+      body: SafeArea(child: _pages[_selectedIndex].widget),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
