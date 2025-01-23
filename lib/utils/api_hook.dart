b693b460aeb26c69e994377f61792009024f8607
charmeee <mandugohigh@gmail.com>
2025-01-24 06:30:46 +0900

    feat: api hook ver1

--- lib/utils/api_hook.dart
+++ lib/utils/api_hook.dart
@@ -1,8 +1,4 @@
-
-
-// class ApiHook extends BaseHook<ApiHookModel> {
-//   //isLoading , error, data , refetch , isCashed
-// }
+import 'base_hook.dart';
 
 class ApiHookModel {
   final bool isLoading;
@@ -18,4 +14,51 @@ class ApiHookModel {
     required this.reFetch,
     required this.isCashed,
   });
+
+  ApiHookModel copyWith({
+    bool? isLoading,
+    bool? error,
+    dynamic data,
+    Function? reFetch,
+    bool? isCashed,
+  }) {
+    return ApiHookModel(
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+      data: data ?? this.data,
+      reFetch: reFetch ?? this.reFetch,
+      isCashed: isCashed ?? this.isCashed,
+    );
+  }
+}
+
+class ApiHook extends BaseHook<ApiHookModel> {
+  ApiHook()
+      : super(ApiHookModel(
+          isLoading: false,
+          error: false,
+          data: null,
+          reFetch: () {},
+          isCashed: false,
+        ));
+
+  void startLoading() {
+    state = state.copyWith(isLoading: true);
+  }
+
+  void setError(bool error) {
+    state = state.copyWith(error: error);
+  }
+
+  void setData(dynamic data) {
+    state = state.copyWith(isLoading: false, error: false, data: data);
+  }
+
+  void setReFetch(Function reFetch) {
+    state = state.copyWith(reFetch: reFetch);
+  }
+
+  void setIsCashed(bool isCashed) {
+    state = state.copyWith(isCashed: isCashed);
+  }
 }
