5f89ba361222bd1f5dc6e732b5282880d04239e0
charmeee <mandugohigh@gmail.com>
2025-02-03 11:34:54 +0900

    feat: history ui refactoring 및 hook적용

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:taesung1/utils/api_hook.dart';
 
 import '../models/history_model.dart';
-import '../services/api_service.dart';
-import '../widgets/common/empty_display_box.dart';
 import '../widgets/common/my_header.dart';
 import '../widgets/screens/history/history.dart';
 
@@ -15,22 +12,8 @@ class HistoryScreen extends StatefulWidget {
 }
 
 class _HistoryScreenState extends State<HistoryScreen> {
-  late final ApiHook historyApiHook;
   DateRange? selectedRange = DateRange.thisMonth;
   DateTime currentMonth = DateTime.now();
-
-  @override
-  void initState() {
-    super.initState();
-    historyApiHook =
-        ApiHook<List<HistoryData>>(apiCall: ApiService.getHistoryData, params: {
-      'start': dateRange.start,
-      'end': dateRange.end,
-    });
-    historyApiHook.addListener(() {
-      setState(() {});
-    });
-  }
 
   DateTimeRange get dateRange =>
       selectedRange?.dateRange ??
@@ -38,45 +21,6 @@ class _HistoryScreenState extends State<HistoryScreen> {
         start: DateTime(currentMonth.year, currentMonth.month, 1),
         end: DateTime(currentMonth.year, currentMonth.month + 1, 0),
       );
-
-  @override
-  void didUpdateWidget(covariant HistoryScreen oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    historyApiHook.updateParams({
-      'start': dateRange.start,
-      'end': dateRange.end,
-    });
-  }
-
-  @override
-  void dispose() {
-    historyApiHook.dispose();
-    super.dispose();
-  }
-
-  // Future<void> _loadHistoryData() async {
-  //   setState(() {
-  //     _isLoading = true;
-  //   });
-  //
-  //   try {
-  //
-  //     final data = await ApiService.getHistoryData(
-  //       start: dateRange.start,
-  //       end: dateRange.end,
-  //     );
-  //
-  //     setState(() {
-  //       records = data;
-  //     });
-  //   } catch (e) {
-  //     print('Error loading history data: $e');
-  //   } finally {
-  //     setState(() {
-  //       _isLoading = false;
-  //     });
-  //   }
-  // }
 
   void _updateMonth(DateTime newMonth) {
     setState(() {
@@ -109,15 +53,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
               setRange: _updateRange,
             ),
             SizedBox(height: 16),
-            if (historyApiHook.state.isLoading)
-              Center(child: CircularProgressIndicator())
-            else if (historyApiHook.state.data.isEmpty)
-              EmptyDisplayBox(
-                icon: Icons.history,
-                text: "기록이 없습니다.",
-              )
-            else
-              HistoryList(records: historyApiHook.state.data),
+            HistoryList(dateRange: dateRange),
           ],
         ),
       ),
