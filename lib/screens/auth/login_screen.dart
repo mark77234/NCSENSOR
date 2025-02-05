35059ce9823b7641c84c25b5f7bd66a756ed17c5
mark <mark77234@naver.com>
2025-02-05 16:36:44 +0900

    Refactor: 로그인 화면 리팩토링

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,10 +1,12 @@
-import 'package:NCSensor/screens/auth/register_screen.dart';
 import 'package:NCSensor/screens/common/main_screen.dart';
+import 'package:NCSensor/widgets/common/error_dialog.dart';
+import 'package:NCSensor/widgets/screens/login/input_field.dart';
+import 'package:NCSensor/widgets/screens/login/register_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../constants/styles.dart';
 import '../../providers/auth_provider.dart';
+import '../../widgets/screens/login/login_button.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -18,7 +20,6 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _idEntered = TextEditingController();
   final TextEditingController _passwordEntered = TextEditingController();
   bool _hideText = true; // 비밀번호 숨김여부
-  String? _errorMessage; // ? ->  null 값을 가질 수 있다.
 
   @override
   void dispose() {
@@ -33,86 +34,6 @@ class _LoginScreenState extends State<LoginScreen> {
     setState(() {
       _hideText = !_hideText;
     });
-  }
-
-  Widget _buildInputField({
-    // 입력 필드
-    required String label,
-    bool isPassword = false,
-    required TextEditingController controller,
-  }) {
-    return Center(
-      child: SizedBox(
-        width: 320,
-        height: 50,
-        child: TextField(
-          controller: controller,
-          obscureText: isPassword ? _hideText : false,
-          // isPassword가 true면 hideText, false면 false 반환 / '*'표시
-          decoration: InputDecoration(
-            labelText: label,
-            labelStyle: const TextStyle(
-              color: ColorStyles.darkgrey,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              borderSide:
-                  const BorderSide(color: ColorStyles.darkgrey, width: 1),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              borderSide:
-                  const BorderSide(color: ColorStyles.darkgrey, width: 2),
-            ),
-            suffixIcon: isPassword // 비밀번호칸인지 확인 / 입력 위젯에 추가할 수 있는 아이콘
-                ? IconButton(
-                    icon: Icon(
-                      _hideText ? Icons.visibility_off : Icons.visibility,
-                      color: ColorStyles.darkgrey,
-                    ),
-                    onPressed: _togglePasswordHide,
-                  )
-                : null,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildLoginButton() {
-    return SizedBox(
-      width: 320,
-      height: 50,
-      child: ElevatedButton(
-        onPressed: _handleLogin,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color(0xFF3B82F6),
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        child: const Text(
-          '로그인',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRegisterButton() {
-    return TextButton(
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const RegisterScreen()),
-        );
-      },
-      child: const Text(
-        '회원가입',
-        style: TextStyle(color: Color(0xFF3B82F6)),
-      ),
-    );
   }
 
   Future<void> _handleLogin() async {
@@ -133,30 +54,12 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _showErrorDialog(String error) {
-    setState(() {
-      _errorMessage = error;
-    });
 
     showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('로그인 실패'),
-          content: Text(
-            _errorMessage ?? '알 수 없는 오류가 발생했습니다.', // null인 경우 해결
-            style: const TextStyle(fontSize: 18, color: Colors.grey),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('확인'),
-            ),
-          ],
-        );
-      },
-    );
+        context: context,
+        builder: (context) => ErrorDialog(
+              errorMessage: error,
+            ));
   }
 
   @override
@@ -167,29 +70,41 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'N.C.SENSOR',
-              style: TextStyle(
-                fontSize: 50,
-                color: Color(0xFF3B82F6),
-                fontFamily: 'DoHyeon',
-                fontWeight: FontWeight.w500,
-              ),
-            ),
+            buildTitle(),
             const SizedBox(height: 40),
-            _buildInputField(label: '아이디', controller: _idEntered),
+            // _buildInputField(label: '아이디', controller: _idEntered),
+            InputField(
+              label: '아이디',
+              controller: _idEntered,
+            ),
             const SizedBox(height: 10),
-            _buildInputField(
+            InputField(
               label: '비밀번호',
               isPassword: true,
               controller: _passwordEntered,
+              hideText: _hideText,
+              togglePasswordHide: _togglePasswordHide,
             ),
             const SizedBox(height: 20),
-            _buildLoginButton(),
+            LoginButton(
+              onPressed: _handleLogin,
+            ),
             const SizedBox(height: 20),
-            _buildRegisterButton(),
+            RegisterButton(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildTitle() {
+    return const Text(
+      'N.C.SENSOR',
+      style: TextStyle(
+        fontSize: 50,
+        color: Color(0xFF3B82F6),
+        fontFamily: 'DoHyeon',
+        fontWeight: FontWeight.w500,
       ),
     );
   }
