26529c1bd4bb04c7de4bd9d5761684b0052117e5
charmeee <mandugohigh@gmail.com>
2025-02-11 12:08:43 +0900

    feat: 스타일 재적용

--- lib/widgets/screens/history/history_item.dart
+++ lib/widgets/screens/history/history_item.dart
@@ -2,6 +2,7 @@ import 'package:NCSensor/models/data/history_model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../../../constants/styles.dart';
 import '../../../models/meta/article_model.dart';
 import '../../common/icon_widget.dart';
 
@@ -18,19 +19,28 @@ class HistoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Section? section = article.findSectionForValue(data.value);
-    return ListTile(
-      leading: _buildHistoryIcon(article.icon),
-      title: Text(article.name),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('${data.value} ${article?.unit ?? ''}'),
-          Text(section?.name ?? '', style: TextStyle(color: section?.color)),
-        ],
-      ),
-      trailing: Text(
-        DateFormat('HH:mm').format(data.datetime),
-        style: TextStyle(color: Colors.grey),
+    return Container(
+      decoration: ContainerStyles.card,
+      margin: EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        leading: _buildHistoryIcon(article.icon),
+        title: Text(article.name,
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+        subtitle: Text('${data.value} ${article?.unit ?? ''}'),
+        trailing: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat('HH:mm').format(data.datetime),
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(height: 4),
+            Text(section?.name ?? '',
+                style: TextStyle(
+                    color: section?.color, fontWeight: FontWeight.bold)),
+          ],
+        ),
       ),
     );
   }
