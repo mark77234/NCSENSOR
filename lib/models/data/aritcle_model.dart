20354e84ef68d83c46ddacacdc71b5cbe6ee5df8
charmeee <mandugohigh@gmail.com>
2025-02-12 13:41:17 +0900

    refactor: 서브타입 제거

--- lib/models/data/aritcle_model.dart
+++ lib/models/data/aritcle_model.dart
@@ -7,9 +7,8 @@ class ArticleData {
 
   factory ArticleData.fromJson(Map<String, dynamic> json) {
     return ArticleData(
-      articles: (json['articles'] as List)
-          .map((e) => Article.fromJson(e))
-          .toList(),
+      articles:
+          (json['articles'] as List).map((e) => Article.fromJson(e)).toList(),
     );
   }
 }
@@ -17,38 +16,14 @@ class ArticleData {
 class Article {
   final String id;
   final String name;
-  final List<Subtype>? subtypes;
 
   Article({
     required this.id,
     required this.name,
-    this.subtypes,
   });
 
   factory Article.fromJson(Map<String, dynamic> json) {
     return Article(
-      id: json['id'],
-      name: json['name'],
-      subtypes: json['subtypes'] != null
-          ? (json['subtypes'] as List)
-          .map((e) => Subtype.fromJson(e))
-          .toList()
-          : null,
-    );
-  }
-}
-
-class Subtype {
-  final String id;
-  final String name;
-
-  Subtype({
-    required this.id,
-    required this.name,
-  });
-
-  factory Subtype.fromJson(Map<String, dynamic> json) {
-    return Subtype(
       id: json['id'],
       name: json['name'],
     );
