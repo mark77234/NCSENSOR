3fb0ada6096f652336346478050dea20bcc026b6
charmeee <mandugohigh@gmail.com>
2025-02-14 15:29:27 +0900

    feat: usb 읽는거 추가

--- lib/screens/measure/widgets/sensor_status_card.dart
+++ lib/screens/measure/widgets/sensor_status_card.dart
@@ -1,13 +1,34 @@
 import 'package:NCSensor/constants/styles.dart';
 import 'package:NCSensor/screens/measure/widgets/status_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:usb_serial/usb_serial.dart';
 
 import '../measure_screen.dart';
 
-class SensorStatusCard extends StatelessWidget {
+class SensorStatusCard extends StatefulWidget {
   final MeasureStatus status;
 
   const SensorStatusCard({super.key, required this.status});
+
+  @override
+  State<SensorStatusCard> createState() => _SensorStatusCardState();
+}
+
+class _SensorStatusCardState extends State<SensorStatusCard> {
+  List<UsbDevice> devices = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readDevice();
+  }
+
+  readDevice() async {
+    devices = await UsbSerial.listDevices();
+    print("Devices: ");
+    print(devices);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +43,7 @@ class SensorStatusCard extends StatelessWidget {
               style: MeasureTextStyles.sub.copyWith(
                 fontSize: 18,
               )),
-          StatusIndicator(status: status),
+          StatusIndicator(status: widget.status),
         ],
       ),
     );
