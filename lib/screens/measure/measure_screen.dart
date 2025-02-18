dd01388c2c9581e9e4323454cea3fd7a635e650e
charmeee <mandugohigh@gmail.com>
2025-02-18 14:47:48 +0900

    feat: 센서 데이터 읽는 것 추가

--- lib/screens/measure/measure_screen.dart
+++ lib/screens/measure/measure_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:NCSensor/screens/result/result_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
 
 import '../../constants/styles.dart';
 import '../../routes/fade_page_route.dart';
@@ -31,16 +36,22 @@ class _MeasureScreenState extends State<MeasureScreen> {
   final int limitSec = 10; // 몇 초 동안 측정하는지
   final double termSec = 1; // 몇 초마다 측정하는지
 
-  static const _testSensors = [
-    {"sensor_id": "1", "value": 0, "measured_at": "2025-01-22T00:00:00"},
-    {"sensor_id": "2", "value": 2, "measured_at": "2025-01-22T00:00:00"},
-    {"sensor_id": "3", "value": 3, "measured_at": "2025-01-22T00:00:00"},
-    {"sensor_id": "4", "value": 5, "measured_at": "2025-01-22T00:00:00"},
+  //센서관련
+  UsbPort? port;
+  Transaction<String>? _transaction;
+  StreamSubscription<String>? _subscription;
+
+  List<Map<String, dynamic>> _testSensors = [
+    // {"sensor_id": "1", "value": 0, "measured_at": "2025-01-22T00:00:00"},
+    // {"sensor_id": "2", "value": 2, "measured_at": "2025-01-22T00:00:00"},
+    // {"sensor_id": "3", "value": 3, "measured_at": "2025-01-22T00:00:00"},
+    // {"sensor_id": "4", "value": 5, "measured_at": "2025-01-22T00:00:00"},
   ];
 
   Future<void> _startMeasurement() async {
-    if (!mounted) return;
+    if (!mounted || measureStatus != MeasureStatus.ready) return;
     setState(() => measureStatus = MeasureStatus.measuring);
+    _startListenSensor();
     for (int i = 1; i <= limitSec; i++) {
       if (!mounted) return;
       int termMilli = (termSec * 1000).toInt(); // 측정 텀을 밀리세크로 변환
@@ -52,12 +63,50 @@ class _MeasureScreenState extends State<MeasureScreen> {
     _navigateToResult();
   }
 
+  Future<void> _startListenSensor() async {
+    if (port == null) return;
+    await port!.setDTR(true);
+    await port!.setRTS(true);
+    await port!.setPortParameters(
+        115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(
+        port!.inputStream as Stream<Uint8List>, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction!.stream.listen((String line) {
+      if (measureStatus == MeasureStatus.done || !mounted) return;
+      _testSensors.add({
+        "sensor_id": "1",
+        "value": line,
+      });
+      setState(() {});
+    });
+  }
+
   void _navigateToResult() {
     Navigator.pushReplacement(
         context,
         FadePageRoute(
             page: ResultScreen(
                 articleId: widget.articleId, sensors: _testSensors)));
+  }
+
+  setMeasureStatus(MeasureStatus status) {
+    if (!mounted) return;
+    setState(() => measureStatus = status);
+  }
+
+  setPort(UsbPort port) {
+    if (!mounted) return;
+    setState(() => this.port = port);
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    _transaction?.dispose();
+    port?.close();
+    super.dispose();
   }
 
   @override
@@ -74,7 +123,11 @@ class _MeasureScreenState extends State<MeasureScreen> {
                 limitSec: limitSec,
                 status: measureStatus,
               ),
-              SensorStatusCard(status: measureStatus),
+              SensorStatusCard(
+                status: measureStatus,
+                setMeasureStatus: setMeasureStatus,
+                setPort: setPort,
+              ),
               ActionButton(
                 status: measureStatus,
                 onNavigateToResult: _navigateToResult,
